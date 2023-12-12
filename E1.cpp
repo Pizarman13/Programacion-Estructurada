4251c88e55ab46093d37fef7732b7f05fa753c72
Pizarman13 <dpizagar@myuax.com>
2023-12-12 21:06:47 +0100

    Creaion de la estructura

---
+++ E1.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Pizarman on 12/12/2023.
+//
+#include <iostream>
+
+// a
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+};
+
+int main() {
+
+    return 0;
+}
